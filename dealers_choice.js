cdfc8278c11caf574d24feab9b15deee9955070f
Stephen Wong <stephen_wong_tx@icloud.com>
2021-07-26 15:38:11 -0500

    committing some changes

--- dealers_choice.js
+++ dealers_choice.js
@@ -15,22 +15,74 @@ app.get("/", (request, response) => {
   <!DOCTYPE html>
   <html lang="en">
     <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Fourteeners</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>The Fourteeners</title>
+      <link rel="stylesheet" href="/style.css" />
     </head>
     <body>
-      <div class="nav">
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li style="float:right"><a class="active" href="#about">About</a></li>
-      </ul>
+      <div id="nav">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li style="float:right"><a class="active" href="#about">About</a></li>
+          </ul>
       </div>
+      <div id="main-content">
       <h1>The Fourtneeners</h1>
+        <div id="entry-list">
+          ${entries.map( entry => `
+            <div class="entryContainer">
+              <h2>${entry["Mountain Peak"]}</h2>
+              <p>Range: ${entry["Mountain Range"]}</p>
+              <p>Eleveation: ${entry.Elevation_ft}</p>
+            </div>
+          `)}
+        </div>
+    </div>
     </body>
   </html>
   `
+  response.send(html);
 })
+
+app.get( '/entries/:ID', (request, response) => {
+  console.log(request.params.ID);
+  const ID = request.params.ID;
+  const entry = postBank.find(ID);
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/style.css" />
+        <title>Fourteeners ID: ${ID}</title>
+      </head>
+      <body>
+        <div id="nav">
+          <img src="14ersHome.png" alt="The number 14 in front of a mountain range drawing">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li style="float:right"><a class="active" href="#about">About</a></li>
+          </ul>
+        </div>
+        <div>
+          <h1 class="h3">Mountain: ${entry["Mountain Peak"]}</h1>
+          <p>Range: ${entry["Mountain Range"]}</p>
+          <p>Elevation: ${entry.Elevation_ft}</p>
+        </div>
+      </body>
+    </html>
+  `
+  response.send(html);
+})
+
+const PORT = 1337;
+
+app.listen(PORT, () => console.log(`App listening in port ${PORT}`));
